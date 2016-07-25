4f46f55c7799bda3d34febfbfe6e291f18f437c9
Casey McCandless <caseymcc@krazer.net>
2016-07-25 08:14:21 -0500

    bug fixes

--- addDependency.cmake
+++ addDependency.cmake
@@ -32,6 +32,23 @@ function(add_dependency DEPENDENCY)
 	set(multiValueArgs "")
 	cmake_parse_arguments(ADD_DEPENDENCY "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN} )
 
+	#set cpm folders
+	if(DEFINED ADD_GIT_DEPENDENCY_GIT_TAG)
+		set(DEPENDENCY_PACKAGE_SOURCE_DIR "${CPM_PACKAGE_DIR}/source/${DEPENDENCY}-${ADD_DEPENDENCY_GIT_TAG}")
+		set(DEPENDENCY_PACKAGE_DIR "${CPM_PACKAGE_DIR}/${DEPENDENCY}-${ADD_DEPENDENCY_GIT_TAG}")
+	else()
+		set(DEPENDENCY_PACKAGE_SOURCE_DIR "${CPM_PACKAGE_DIR}/source/${DEPENDENCY}")
+		set(DEPENDENCY_PACKAGE_DIR "${CPM_PACKAGE_DIR}/${DEPENDENCY}")
+	endif()
+
+
+	get_filename_component(DEPENDENCY_PACKAGE_DIR ${DEPENDENCY_PACKAGE_DIR} ABSOLUTE)
+
+	if(NOT DEFINED ADD_GIT_DEPENDENCY_GIT_TAG) #not set origin master
+		set(ADD_GIT_DEPENDENCY_GIT_TAG "origin/master")
+	endif()
+
+
 	set(CPM_LOCAL_PACKAGE "${CMAKE_MODULE_PATH}/cpmPackage${DEPENDENCY}.cmake")
 	if(EXISTS ${CPM_LOCAL_PACKAGE})
 		include(${CPM_LOCAL_PACKAGE})
@@ -52,10 +69,11 @@ function(add_dependency DEPENDENCY)
 #			set(ADD_DEPENDENCY_GIT_TAG "origin/master")
 #		endif()
 #
-#		message(STATUS "Pkg Source Dir: ${DEPENDENCY_PACKAGE_SOURCE_DIR}")
-#		message(STATUS "Pkg Dir: ${DEPENDENCY_PACKAGE_DIR}")
-#		message(STATUS "Git Repo: ${ADD_DEPENDENCY_GIT_REPOSITORY}")
-#		message(STATUS "Git Tag: ${ADD_DEPENDENCY_GIT_TAG}")
+		message(STATUS "add_dependency: ${DEPENDENCY}")
+		message(STATUS "Pkg Source Dir: ${DEPENDENCY_PACKAGE_SOURCE_DIR}")
+		message(STATUS "Pkg Dir: ${DEPENDENCY_PACKAGE_DIR}")
+		message(STATUS "Git Repo: ${ADD_DEPENDENCY_GIT_REPOSITORY}")
+		message(STATUS "Git Tag: ${ADD_DEPENDENCY_GIT_TAG}")
 #
 #		CPM_EnsureRepoIsCurrent(TARGET_DIR ${DEPENDENCY_PACKAGE_SOURCE_DIR}
 #			GIT_REPOSITORY ${ADD_DEPENDENCY_GIT_REPOSITORY}
@@ -64,14 +82,13 @@ function(add_dependency DEPENDENCY)
 #		)
 #
 #		add_subdirectory(${DEPENDENCY_PACKAGE_SOURCE_DIR} ${DEPENDENCY_PACKAGE_DIR})
-		add_git_dependency(DEPENDENCY_PACKAGE_DIR ${DEPENDENCY} ${ADD_DEPENDENCY_GIT_REPOSITORY} ${ADD_DEPENDENCY_GIT_TAG})
+		add_git_dependency(${DEPENDENCY} ${ADD_DEPENDENCY_GIT_REPOSITORY} GIT_TAG ${ADD_DEPENDENCY_GIT_TAG})
 		set(${DEPENDENCY}_INCLUDE "${DEPENDENCY_PACKAGE_DIR}/include")
 	endif()
 
 endfunction(add_dependency)
 
-function(add_git_dependency ${} DEPENDENCY GIT_REPOSITORY)
-{
+function(add_git_dependency DEPENDENCY GIT_REPOSITORY)
 	set(options "")
 	set(oneValueArgs GIT_TAG)
 	set(multiValueArgs "")
@@ -90,6 +107,7 @@ function(add_git_dependency ${} DEPENDENCY GIT_REPOSITORY)
 		set(ADD_GIT_DEPENDENCY_GIT_TAG "origin/master")
 	endif()
 
+	message(STATUS "add_git_dependency: ${DEPENDENCY} ${GIT_REPOSITORY}")
 	message(STATUS "Pkg Source Dir: ${DEPENDENCY_PACKAGE_SOURCE_DIR}")
 	message(STATUS "Pkg Dir: ${DEPENDENCY_PACKAGE_DIR}")
 	message(STATUS "Git Repo: ${GIT_REPOSITORY}")
@@ -103,4 +121,4 @@ function(add_git_dependency ${} DEPENDENCY GIT_REPOSITORY)
 
 	add_subdirectory(${DEPENDENCY_PACKAGE_SOURCE_DIR} ${DEPENDENCY_PACKAGE_DIR})
 	set( ${DEPENDENCY_PACKAGE_DIR})
-}
+endfunction(add_git_dependency)
--- packages/cpmPackageGLFW.cmake
+++ packages/cpmPackageGLFW.cmake
@@ -1,9 +1,21 @@
+message(STATUS "GLFW -------------------------------------")
+message(STATUS "add_dependency: ${DEPENDENCY}")
+message(STATUS "Pkg Source Dir: ${DEPENDENCY_PACKAGE_SOURCE_DIR}")
+message(STATUS "Pkg Dir: ${DEPENDENCY_PACKAGE_DIR}")
+message(STATUS "Git Repo: ${ADD_DEPENDENCY_GIT_REPOSITORY}")
+message(STATUS "Git Tag: ${ADD_DEPENDENCY_GIT_TAG}")
+message(STATUS "------------------------------------------")
+
 set(GLFW_BUILD_EXAMPLES OFF)
 set(GLFW_BUILD_TESTS OFF)
 set(GLFW_INSTALL OFF)
 
-add_git_dependency(${DEPENDENCY} "https://github.com/glfw/glfw.git" ${ADD_DEPENDENCY_GIT_TAG})
+add_git_dependency(${DEPENDENCY} "https://github.com/glfw/glfw.git" GIT_TAG ${ADD_DEPENDENCY_GIT_TAG})
+
+message(STATUS "GLFW: pkg dir: ${DEPENDENCY_PACKAGE_SOURCE_DIR}")
+message(STATUS "GLFW: source dir: ${DEPENDENCY_PACKAGE_DIR}")
 
 add_subdirectory(${DEPENDENCY_PACKAGE_SOURCE_DIR} ${DEPENDENCY_PACKAGE_DIR})
-		
+	
+set_target_properties(glfw PROPERTIES FOLDER "cpmPackages")
 set(${DEPENDENCY}_INCLUDE "${DEPENDENCY_PACKAGE_DIR}/include")
