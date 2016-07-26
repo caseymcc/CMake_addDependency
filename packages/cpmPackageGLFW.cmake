8955e25b72e9127b1784ef2cd319060c6b27198b
Casey McCandless <caseymcc@krazer.net>
2016-07-26 13:32:02 -0500

    separated build folder from source and install using ExternalProject_Add to add project to build

--- packages/cpmPackageGLFW.cmake
+++ packages/cpmPackageGLFW.cmake
@@ -1,10 +1,11 @@
-message(STATUS "GLFW -------------------------------------")
-message(STATUS "add_dependency: ${DEPENDENCY}")
-message(STATUS "Pkg Source Dir: ${DEPENDENCY_PACKAGE_SOURCE_DIR}")
-message(STATUS "Pkg Dir: ${DEPENDENCY_PACKAGE_DIR}")
-message(STATUS "Git Repo: ${ADD_DEPENDENCY_GIT_REPOSITORY}")
-message(STATUS "Git Tag: ${ADD_DEPENDENCY_GIT_TAG}")
-message(STATUS "------------------------------------------")
+#message(STATUS "GLFW -------------------------------------")
+#message(STATUS "add_dependency: ${DEPENDENCY}")
+#message(STATUS "Pkg Source Dir: ${DEPENDENCY_PACKAGE_SOURCE_DIR}")
+#message(STATUS "Pkg Build Dir: ${DEPENDENCY_PACKAGE_BUILD_DIR}")
+#message(STATUS "Pkg Install Dir: ${DEPENDENCY_PACKAGE_INSTALL_DIR}")
+#message(STATUS "Git Repo: ${ADD_DEPENDENCY_GIT_REPOSITORY}")
+#message(STATUS "Git Tag: ${ADD_DEPENDENCY_GIT_TAG}")
+#message(STATUS "------------------------------------------")
 
 set(GLFW_BUILD_EXAMPLES OFF)
 set(GLFW_BUILD_TESTS OFF)
@@ -12,10 +13,8 @@ set(GLFW_INSTALL OFF)
 
 add_git_dependency(${DEPENDENCY} "https://github.com/glfw/glfw.git" GIT_TAG ${ADD_DEPENDENCY_GIT_TAG})
 
-message(STATUS "GLFW: pkg dir: ${DEPENDENCY_PACKAGE_SOURCE_DIR}")
-message(STATUS "GLFW: source dir: ${DEPENDENCY_PACKAGE_DIR}")
+#message(STATUS "GLFW: build dir: ${DEPENDENCY_PACKAGE_BUILD_DIR}")
+#message(STATUS "GLFW: source dir: ${DEPENDENCY_PACKAGE_SOURCE_DIR}")
 
-add_subdirectory(${DEPENDENCY_PACKAGE_SOURCE_DIR} ${DEPENDENCY_PACKAGE_DIR})
-	
-set_target_properties(glfw PROPERTIES FOLDER "cpmPackages")
-set(${DEPENDENCY}_INCLUDE "${DEPENDENCY_PACKAGE_DIR}/include")
+set_target_properties(glfw PROPERTIES FOLDER "packages")
+set(${DEPENDENCY}_INCLUDE "${DEPENDENCY_PACKAGE_INSTALL_DIR}/include")
